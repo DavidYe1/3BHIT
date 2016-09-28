15eb8a5673fd2496f22442562a25a374362336f0
David Ye <david.ye@live.de>
2016-09-28 20:34:07 +0200

    ArrayTest

--- Utitlity/src/IntArrayTest.java
+++ Utitlity/src/IntArrayTest.java
@@ -1,16 +1,27 @@
 import java.util.Random;
 
 /**
+ * testet alle Utility methoden die arrays verwenden
  * Created by @Autor david ye on @version 28.09.2016.
  */
 public class IntArrayTest {
-    private static int[] testArray1 = new int[10];
+    private static int[] testArray1 = new int[10]; //10 stelliges array erstellen
 
     public static void main(String[] args) {
-        randomFill(testArray1);
+        randomFill(testArray1);// array füllen
         System.out.print(Utility.max(testArray1));
-    }
-
+    }/**
+     Fehler
+     method Utility.<T>random(T[]) is not applicable
+     (inferred type does not conform to upper bound(s)
+     inferred: int
+     upper bound(s): java.lang.Object)
+     */
+    /**
+     * Erstellt random zahlen und füllt ein array damit
+     *
+     * @param ar1
+     */
     public static void randomFill(int[] ar1) {
         Random r = new Random();
         for (int i = 0; i < ar1.length; i++) {

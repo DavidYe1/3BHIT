2867188334e2fabcab35729cd12154a124cb3a1b
David Ye <david.ye@live.de>
2016-09-28 18:07:55 +0200

    comments

--- Utitlity/src/IntTest.java
+++ Utitlity/src/IntTest.java
@@ -2,16 +2,22 @@
  * Created by David Ye on 22.09.2016.
  */
 public class IntTest {
-    private static int testvar1 = (int) (Math.random() * 100);
-    private static int testvar2 = (int) (Math.random() * 100);
-    private static int fixvar1 = 8;
-    private static int fixvar2 = 12;
+    /**
+     * variable deklarieren und initialisieren
+     */
+    private static int testvar1 = (int) (Math.random() * 100); //zufallszahl
+    private static int testvar2 = (int) (Math.random() * 100); //zufallszahl
+    private static int fixvar1 = 8; //fixwert
+    private static int fixvar2 = 12;//fixwert
 
+    /**
+     * @param args testen aller methoden die kein array verlangen
+     */
     public static void main(String[] args) {
-        System.out.print(Utility.random(fixvar1, fixvar2) + " wurde zufaellig ausgewählt.\n" +
-                Utility.max(testvar1, testvar2) + " ist groeser als " + Utility.min(testvar1, testvar2) + "\n" +
-                "Die aus sage " + fixvar1 + "=" + fixvar2 + " ist " + ((Utility.equalTo(fixvar1, fixvar2)) ? "wahr." : "falsch.") + "\n" +
-                "Die aus sage " + fixvar1 + "=" + fixvar1 + " ist " + ((Utility.equalTo(fixvar1, fixvar1)) ? "wahr." : "falsch.") + "\n" +
-                testvar1 + "ist " + ((Utility.isOdd(testvar1)) ? "ungerade." : "gerade."));
+        System.out.print(Utility.random(fixvar1, fixvar2) + " wurde zufaellig ausgewählt.\n" + //nimmt 2 fixwerte und gibt 1 zufällig zurück
+                Utility.max(testvar1, testvar2) + " ist groeser als " + Utility.min(testvar1, testvar2) + "\n" + //vergleicht 2 zugällige zahlen
+                "Die aus sage " + testvar1 + "=" + testvar2 + " ist " + ((Utility.equalTo(testvar1, testvar2)) ? "wahr." : "falsch.") + "\n" + //vergleicht ob 2 zufällige zahlen gleich sind
+                "Die aus sage " + fixvar1 + "=" + fixvar1 + " ist " + ((Utility.equalTo(fixvar1, fixvar1)) ? "wahr." : "falsch.") + "\n" +  //sollte immer true zurück geben
+                testvar1 + "ist " + ((Utility.isOdd(testvar1)) ? "ungerade." : "gerade."));//schaut of eine zufallszahl ungerade ist
     }
 }

ad61461a91e054cb01a1792234334ce29ec6563f
David <david.ye@live.de>
2016-09-29 14:22:37 +0200

    1st push

--- Queue/src/MyLinkedQueue.java
+++ Queue/src/MyLinkedQueue.java
@@ -1,5 +1,16 @@
 /**
  * Created by Keqin on 29.09.2016.
  */
-public class MyLinkedQueue {
+public class MyLinkedQueue<E> implements Iterable<E> {
+
+    private class Cell {
+        private Cell next;
+        private E e;
+
+        public Cell(E e, Cell next) {
+            this.e = e;
+            this.next = next;
+        }
+    }
+
 }
